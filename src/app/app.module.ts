60a97959b79bbda3170dc99cbfdb8493f66267a6
Mr.Fo2sh <m.fouad2912@gmail.com>
2018-01-17 20:47:29 +0200

    first commint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContinueComponent } from './components/continue/continue.component';
+import { IndexComponent } from './components/index/index.component';
+import { QuestionsComponent } from './components/questions/questions.component';
 
+const appRoutes: Routes = [
+  { path: 'continue', component: ContinueComponent },
+  { path: 'questions', component: QuestionsComponent },
+  { path: '**', component: IndexComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContinueComponent,
+    IndexComponent,
+    QuestionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
